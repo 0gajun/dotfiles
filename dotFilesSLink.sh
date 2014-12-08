625a5ab0b74520c96cc10493beb795b64e7c2a04
Ogasawara <oga.ivc.s27@gmail.com>
2014-12-08 17:58:52 +0900

    fix tmp-directory bug

--- dotFilesSLink.sh
+++ dotFilesSLink.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 #作業ディレクトリ作成
-mkdir ~/tmp
+mkdir ~/tmp-dotfiles
 
 #シンボリックリンク作成
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 #For vim
 #neobundleのインストール
-if [ -e ~/.vim/bundle ] ; then
-else
+if [ ! -e ~/.vim/bundle ] ; then
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 fi
 #Solarizedのインストール
-if [ -e ~/.vim/colors/solarized/vim ] ; then
-else
-git clone https://github.com/altercation/vim-colors-solarized.git ~/tmp/vim-colors-solarized/
+if [ ! -e ~/.vim/colors/solarized/vim ] ; then
+git clone https://github.com/altercation/vim-colors-solarized.git ~/tmp-dotfiles/vim-colors-solarized/
 cp -r ~/tmp/vim-colors-solarized/* ~/.vim/
 fi
 
 #作業ディレクトリの削除
-rm -rf ~/tmp
+rm -rf ~/tmp-dotfiles
