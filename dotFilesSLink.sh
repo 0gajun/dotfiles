66669f7ea283c1180f1f24f283f8de47fe292d81
Ogasawara <oga.ivc.s27@gmail.com>
2014-12-14 00:22:46 +0900

    bug fix and add installing oh-my-zsh

--- dotFilesSLink.sh
+++ dotFilesSLink.sh
@@ -12,8 +12,12 @@ fi
 #Solarizedのインストール
 if [ ! -e ~/.vim/colors/solarized/vim ] ; then
 git clone https://github.com/altercation/vim-colors-solarized.git ~/tmp-dotfiles/vim-colors-solarized/
-cp -r ~/tmp/vim-colors-solarized/* ~/.vim/
+cp -r ~/tmp-dotfiles/vim-colors-solarized/* ~/.vim/
 fi
+
+#For zsh
+#oh-my-zshのインストール
+curl -L http://install.ohmyz.sh | sh
 
 #作業ディレクトリの削除
 rm -rf ~/tmp-dotfiles
