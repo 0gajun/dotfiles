66cfb1f72c59e117d0de012b3970e7f6de4ac97c
oga <oga.ivc.s27@gmail.com>
2016-02-29 03:55:25 +0900

    create initial xmonad's configuration file.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,68 @@
+import System.IO
+import XMonad
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+
+import XMonad.Layout
+import XMonad.Layout.DragPane
+import XMonad.Layout.Gaps
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Gaps
+import XMonad.Layout.Gaps
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Spacing
+import XMonad.Layout.ToggleLayouts
+
+import XMonad.Util.Run
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.SpawnOnce
+
+import XMonad.Prompt
+import XMonad.Prompt.Window
+
+-- Color Setting
+colorBlue      = "#9fc7e8"
+colorGreen     = "#a5d6a7"
+colorRed       = "#ef9a9a"
+colorGray      = "#9e9e9e"
+colorWhite     = "#ffffff"
+colorGrayAlt   = "#eceff1"
+colorNormalbg  = "#1c1c1c"
+colorfg        = "#9fa8b1"
+
+main :: IO()
+
+main = do
+  statusBar <- spawnPipe myStatusBar
+  
+  xmonad $ ewmh defaultConfig
+	  { borderWidth = 2
+    , terminal = "urxvt"
+    , normalBorderColor = colorGray 
+    , focusedBorderColor = colorGreen
+    , logHook = myLogHook statusBar
+    , layoutHook = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout }
+
+
+myLayout = (spacing 18 $ ResizableTall 1 (3/100) (3/5) [])
+            ||| (spacing 2 $ (dragPane Horizontal (1/10) (1/2)))
+            ||| (dragPane Vertical (1/10) (1/2))
+
+myLogHook h = dynamicLogWithPP $ statusPP { ppOutput = hPutStrLn h }
+
+myStatusBar = "xmobar $HOME/.xmonad/xmobarrc"
+
+statusPP = xmobarPP { ppOrder           = \(ws:l:t:_)  -> [ws,t]
+                    , ppCurrent         = xmobarColor colorGreen colorNormalbg . \s -> "●"
+                    , ppUrgent          = xmobarColor colorfg    colorNormalbg . \s -> "●"
+                    , ppVisible         = xmobarColor colorfg    colorNormalbg . \s -> "●"
+                    , ppHidden          = xmobarColor colorfg    colorNormalbg . \s -> "●"
+                    , ppHiddenNoWindows = xmobarColor colorfg    colorNormalbg . \s -> "◯"
+                    , ppTitle           = xmobarColor colorGreen colorNormalbg
+                    , ppOutput          = putStrLn
+                    , ppWsSep           = " "
+                    , ppSep             = "  "
+                    }
