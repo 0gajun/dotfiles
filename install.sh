fb6ec16f3be42f47d85860ef97677f60bd9d5986
oga <oga.ivc.s27@gmail.com>
2016-02-29 04:09:33 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -2,31 +2,51 @@
 TMP_DIR=~/tmp-dotfiles
 DOTFILES_DIR=$(cd $(dirname $0);pwd)
 
-#作業ディレクトリ作成
+# when error occurs, stop task
+set -e
+
+# create working directory
 mkdir $TMP_DIR
 
-#シンボリックリンク作成
+# create symlinks
 ln -sf $DOTFILES_DIR/vimrc ~/.vimrc
 ln -sf $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES_DIR/zshrc ~/.zshrc
 
-#For vim
-#neobundleのインストール
+# install xmonad's configuration
+if [ `which xmonad` ] ; then
+  if [ ! -e ~/.xmonad ] ; then
+    mkdir ~/.xmonad
+  fi
+  ln -sf $DOTFILES_DIR/xmonad/* ~/.xmonad/
+fi
+
+# For vim
+# install neobundle
 if [ ! -e ~/.vim/bundle ] ; then
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+else
+  echo "neobundle is already installed"
 fi
-#Solarizedのインストール
-if [ ! -e ~/.vim/colors/solarized/vim ] ; then
-git clone https://github.com/altercation/vim-colors-solarized.git $TMP_DIR/vim-colors-solarized/
-cp -r $TMP_DIR/vim-colors-solarized/* ~/.vim/
+
+# install Solarized color scheme for vim
+if [ ! -e ~/.vim/colors/solarized.vim ] ; then
+  git clone https://github.com/altercation/vim-colors-solarized.git $TMP_DIR/vim-colors-solarized/
+  cp -r $TMP_DIR/vim-colors-solarized/* ~/.vim/
+else
+  echo "Solarized is already installed"
 fi
 
 #For zsh
-#oh-my-zshのインストール
-curl -L http://install.ohmyz.sh | sh
+# install oh-my-zsh
+if [ ! -e ~/.oh-my-zsh ] ; then
+  curl -L http://install.ohmyz.sh | sh
+else
+  echo "oh-my-zsh is already installed"
+fi
 
 # install zsh-theme
 cp $DOTFILES_DIR/zsh-theme/honukai.zsh-theme ~/.oh-my-zsh/themes/
 
-#作業ディレクトリの削除
+# remove working directory
 rm -rf $TMP_DIR
