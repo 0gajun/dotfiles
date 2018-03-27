93d366c43db9930461a522ba7d8e7d8762d85f14
Junya Ogasawara <oga.ivc.s27@gmail.com>
2018-03-27 15:27:21 +0900

    Add direnv hook

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -21,3 +21,7 @@ export GHQ_ROOT=$GOPATH/src
 if [ -f $(brew --prefix)/etc/brew-wrap ];then
   source $(brew --prefix)/etc/brew-wrap
 fi
+
+if type "direnv" > /dev/null ; then
+  eval "$(direnv hook zsh)"
+fi
